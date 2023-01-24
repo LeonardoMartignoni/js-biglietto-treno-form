ae8058d5494a78773b6e87590e38d4fb7a9cf9a1
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-01-24 15:27:06 +0100

    Done Structure

--- js/main.js
+++ js/main.js
@@ -1,30 +1,70 @@
-// Chiedo all'utente quanti Km vuole fare
-const userKm = document.getElementById('user_km');
-console.log(userKm);
+let submitButton = document.getElementById('send_button');
 
-// Chiedo all'utente quanti anni ha
-const userAge = document.getElementById('user_age');
-console.log(userKm);
+submitButton.addEventListener(
+    'click',
 
-// Fisso il prezzo per km (0.21€)
-const pricePerKm = 0.21;
-let ticketPrice = 0;
+    function () {
+        const userKm = document.getElementById('user_km').value;
+        console.log(userKm);
 
-// Se l'utente ha meno di 18 anni applico sconto del 20%
-if (userAge < 18) {
-    ticketPrice = userKm * pricePerKm;
-    const discountPerc = (ticketPrice / 100) * 20;
-    ticketPrice = ticketPrice - discountPerc;
-}
+        // Chiedo all'utente quanti anni ha
+        const userAge = document.getElementById('user_age').value;
+        console.log(userAge);
 
-// Se l'utente ha più di 65 anni applico sconto del 40%
-else if (userAge >= 65) {
-    ticketPrice = userKm * pricePerKm;
-    const discountPerc = (ticketPrice / 100) * 40;
-    ticketPrice = ticketPrice - discountPerc;
-}
+        // Fisso il prezzo per km (0.21€)
+        const pricePerKm = 0.21;
+        let ticketPrice = 0;
 
-// Altrimenti non applico nessuno sconto
-else {
-    ticketPrice = userKm * pricePerKm;
-}
+        // Se l'utente ha meno di 18 anni applico sconto del 20%
+        if (userAge < 18) {
+            ticketPrice = userKm * pricePerKm;
+            const discountPerc = (ticketPrice / 100) * 20;
+            ticketPrice = ticketPrice - discountPerc;
+        }
+
+        // Se l'utente ha più di 65 anni applico sconto del 40%
+        else if (userAge >= 65) {
+            ticketPrice = userKm * pricePerKm;
+            const discountPerc = (ticketPrice / 100) * 40;
+            ticketPrice = ticketPrice - discountPerc;
+        }
+
+        // Altrimenti non applico nessuno sconto
+        else {
+            ticketPrice = userKm * pricePerKm;
+        }
+
+        document.getElementById('ticket_price').innerHTML = ticketPrice.toFixed(2);
+        console.log(ticketPrice.toFixed(2));
+    }
+)
+
+
+// // Chiedo all'utente quanti Km vuole fare
+// const userKm = document.getElementById('user_km');
+
+// // Chiedo all'utente quanti anni ha
+// const userAge = document.getElementById('user_age');
+
+// // Fisso il prezzo per km (0.21€)
+// const pricePerKm = 0.21;
+// let ticketPrice = 0;
+
+// // Se l'utente ha meno di 18 anni applico sconto del 20%
+// if (userAge < 18) {
+//     ticketPrice = userKm * pricePerKm;
+//     const discountPerc = (ticketPrice / 100) * 20;
+//     ticketPrice = ticketPrice - discountPerc;
+// }
+
+// // Se l'utente ha più di 65 anni applico sconto del 40%
+// else if (userAge >= 65) {
+//     ticketPrice = userKm * pricePerKm;
+//     const discountPerc = (ticketPrice / 100) * 40;
+//     ticketPrice = ticketPrice - discountPerc;
+// }
+
+// // Altrimenti non applico nessuno sconto
+// else {
+//     ticketPrice = userKm * pricePerKm;
+// }
